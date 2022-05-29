c8a81247a941106020e7474a0fbdb3fcf1d81694
Positivefan <mrgranze@yandex.ru>
2022-05-29 04:13:35 +0300

    dz 2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,13 +2,40 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+
+    context = {
+        'user_list': [
+            {
+                'first_name': 'NIKOLAY',
+                'last_name': 'IWANOWICH',
+                'age': 31
+            },
+            {
+                'first_name': 'STEPAN',
+                'last_name': 'KAPUSTA',
+                'age': 88
+            },
+
+        ]
+    }
+
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'title': 'все'},
+        {'href': 'products_home', 'title': 'дом'},
+        {'href': 'products_office', 'title': 'офис'},
+        {'href': 'products_modern', 'title': 'модерн'},
+        {'href': 'products_classic', 'title': 'классика'},
+    ]
+
+    context = {
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
-
