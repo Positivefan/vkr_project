c858957608c4aa2cc339d2e702187b3f126047af
Positivefan <mrgranze@yandex.ru>
2022-05-30 19:29:03 +0300

    dz 3

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from django.conf import settings
 from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', views.index, name='index'),
-    path('products/', views.products, name='products'),
+
+    path('products/', include('mainapp.urls', namespace='products')),
+
     path('contact/', views.contact, name='contact'),
 
     path('products/all', views.products, name='products_all'),
@@ -32,3 +37,6 @@ urlpatterns = [
 
     # path('', views.index),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,41 +1,30 @@
 from django.shortcuts import render
+from mainapp.models import Product, Category
 
 
 def index(request):
 
     context = {
-        'user_list': [
-            {
-                'first_name': 'NIKOLAY',
-                'last_name': 'IWANOWICH',
-                'age': 31
-            },
-            {
-                'first_name': 'STEPAN',
-                'last_name': 'KAPUSTA',
-                'age': 88
-            },
-
-        ]
+        'products': Product.objects.all()[:4]
     }
 
     return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    links_menu = [
-        {'href': 'products_all', 'title': 'все'},
-        {'href': 'products_home', 'title': 'дом'},
-        {'href': 'products_office', 'title': 'офис'},
-        {'href': 'products_modern', 'title': 'модерн'},
-        {'href': 'products_classic', 'title': 'классика'},
-    ]
 
     context = {
-        'links_menu': links_menu
+        'links_menu': Category.objects.all()
     }
     return render(request, 'mainapp/products.html', context)
 
+
+def products_list(request, pk):
+    print(pk)
+    context = {
+        'links_menu': Category.objects.all()
+    }
+    return render(request, 'mainapp/products.html', context)
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
